c3a71bbdf03dbb80d996c466f7d234cb1f2b767a
Jon Packer <contact@jonpacker.com>
2013-09-25 16:50:45 +0200

    Added implementation

---
+++ csc.js
@@ -0,0 +1,21 @@
+var url = require('url');
+module.exports = function(config, domain) {
+  if (!config[domain]) throw new Error(domain + ' is not a valid domain');
+
+  config = JSON.parse(JSON.stringify(config));
+
+  var services = Object.create(config);
+
+  Object.keys(config).forEach(function(key) {
+    if (typeof config[key] != 'object') return;
+    config[key].__proto__ = services;
+    if (config[key].port == null) return;
+    services[key] = url.format({
+      port: config[key].port,
+      hostname: config.internalServiceHostname || 'localhost',
+      protocol: config.internalServiceProtocol || 'http'
+    });
+  });
+
+  return config[domain];
+};
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 var csc = require('../');
 var assert = require('assert');
 
-var config = {
+var lolconfig = {
   "neo4j": "http://localhost:7474/",
   "internalServiceHostname": "localhost",
   "internalServiceProtocol": "http",
@@ -25,26 +25,26 @@ var config = {
 
 describe('CSC', function() {
   it('should access basic config settings', function() {
-    var config = csc(config, 'stout');
+    var config = csc(lolconfig, 'stout');
     assert.equal(config.port, 6005);
     assert.equal(config.cookieDomain, "localhost");
     assert.equal(config.defaultUrl, "http://lol.com");
   });
   it('should inherit values on the root object', function() {
-    var config = csc(config, 'stout');
-    assert.equal(config.neo4j, 'http://localhost:7474');
+    var config = csc(lolconfig, 'stout');
+    assert.equal(config.neo4j, 'http://localhost:7474/');
   });
   it('should inherit non-service objects on the root object', function() {
-    var config = csc(config, 'stout');
+    var config = csc(lolconfig, 'stout');
     assert.equal(config.stoutmeal.cookie, 'authKey');
     assert.equal(config.stoutmeal.store.type, 'redis');
   });
   it('should transform root service objects to their location', function() {
-    var config = csc(config, 'stout');
+    var config = csc(lolconfig, 'stout');
     assert.equal(config.sahti, 'http://localhost:6003');
   });
   it('non-service root objects should also inherit service locations', function() {
-    var config = csc(config, 'stout');
+    var config = csc(lolconfig, 'stout');
     assert.equal(config.stoutmeal.stout, 'http://localhost:6005');
   });
 });
